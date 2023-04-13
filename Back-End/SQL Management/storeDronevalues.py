72c9cb97dcbb0b81d9858fd5d11f27ba852c4325
carson375 <carson.375@osu.edu>
2023-04-13 12:16:16 -0400

    Structure Update
    Laid out all the files for the backend

---
+++ Back-End/SQL Management/storeDronevalues.py
@@ -0,0 +1,11 @@
+# I will be given a CSV with n rows by 3 columns: x, y, wifi strength
+# I will need to make point objects for each of the points
+
+
+# First thing to do is query the csv data points
+
+# Next thing to do is connect to the SQL Database
+
+# Next thing to do is insert flight no and user id into database
+
+# Next thing to do is insert the actual data
