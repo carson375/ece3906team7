0c1a686c1ec07f1218e23a48e882432498cfe27c
carson375 <carson.375@osu.edu>
2023-04-16 19:10:04 -0400

    Update storeDronevalues.py

--- Back-End/SQL Management/storeDronevalues.py
+++ Back-End/SQL Management/storeDronevalues.py
@@ -1,5 +1,8 @@
 # I will be given a CSV with n rows by 3 columns: x, y, wifi strength
 # I will need to make point objects for each of the points
+# For this these will be queried from the front end
+userId = 5102
+flightNo = 3
 
 
 # First thing to do is query the csv data points
